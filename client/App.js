bf5ca1db64caa40b17d55c02e8d634279b94aef5
Meg Shulmister <megshulmister@Megs-MacBook-Pro.local>
2021-02-12 13:48:53 -0700

    Styling landing page

--- client/App.js
+++ client/App.js
@@ -1,10 +1,23 @@
+import { Row, Col, Grid, Container } from 'react-bootstrap';
 import React from 'react';
 import './portfolio.css'
+
 
 const App = () => {
     return (
         <div className="App">
-            Welcome to my portfolio
+         <Container >
+            
+          
+
+            <h1 className="name">I'm Meg Shulmister</h1>
+            <br></br>
+            <span className="name">
+            I build beautiful websites. 
+            </span>
+            {/* <div className="nav"> */}
+            {/* </div> */}
+        </Container>
         </div>
     );
 };
