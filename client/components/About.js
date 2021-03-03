6d12324ccf16204f9f39a5da1c4e8e7ed1a3029a
Meg Shulmister <megshulmister@Megs-MacBook-Pro.local>
2021-03-03 16:22:28 -0700

    Working on about section

--- client/components/About.js
+++ client/components/About.js
@@ -12,7 +12,9 @@ function About(props) {
                 <Col className="justify-content-center info">
                     <h2>Thank you for being here.</h2>
                     <br></br>
-                    
+                    <h3>I'm a Front-end Developer.</h3>
+                    <br></br>
+                    <h3>I am inspired by the natural world around me</h3>
             </Col>
             <Col style={{backgroundColor: 'blue'}}>
             Photo
