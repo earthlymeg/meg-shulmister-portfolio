9bb1a11e6c0de7066f2b50e0f10f1fe1176883aa
Meg Shulmister <megshulmister@Megs-MacBook-Pro.local>
2021-03-03 11:31:45 -0700

    Added media queries for responsive desgin

--- client/App.js
+++ client/App.js
@@ -1,4 +1,4 @@
-import { Row, Col, Grid, Container } from 'react-bootstrap';
+import { Row, Col, Grid, Container, navbar } from 'react-bootstrap';
 import React from 'react';
 import './portfolio.css'
 
@@ -6,18 +6,41 @@ import './portfolio.css'
 const App = () => {
     return (
         <div className="App">
-         <Container >
-            
-          
-
-            <h1 className="name">I'm Meg Shulmister</h1>
-            <br></br>
-            <span className="name">
-            I build beautiful websites. 
-            </span>
-            {/* <div className="nav"> */}
-            {/* </div> */}
-        </Container>
+            <Container >
+                <nav class="navbar navbar-light bg-dark">
+                    <a class="navbar-brand" href="#">
+                        Home
+                    </a>
+                    <a class="navbar-brand" href="#about">
+                        About
+                    </a>
+                    <a class="navbar-brand" href="#projects">
+                        Projects
+                    </a>
+                    <a class="navbar-brand" href="#contact">
+                        Contact
+                    </a>
+                </nav>
+                <nav class="navbar fixed-bottom header justify-content-end">
+                <span className="header">I'm <br></br>Meg <br></br>Shulmister</span>
+                </nav>
+                {/* <section id="about">
+                    This is my about zone
+                </section>
+                <section id="projects">
+                    This is my projects zone
+                </section>
+                <section id="contact">
+                    This is my contact zone
+                </section>
+                 */}
+                
+                
+                
+        
+                {/* <div className="nav"> */}
+                {/* </div> */}
+            </Container>
         </div>
     );
 };
