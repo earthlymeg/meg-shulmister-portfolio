29642eb3a0bd95a8f03106a02ba9dff816de8129
Meg Shulmister <megshulmister@Megs-MacBook-Pro.local>
2021-03-09 16:22:32 -0700

    Formatting navbar

--- client/App.js
+++ client/App.js
@@ -10,7 +10,6 @@ const App = () => {
 
     const [currentY, setcurrentY] = useState(0);
     const [headerView, setheaderView] = useState(true);
-    const [expanded, setExpanded] = useState(false);
 
 
 
@@ -59,26 +58,17 @@ const App = () => {
                                     </ul>
                                 </div>
                             </nav> */}
-                            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-                                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                            <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+                                {/* <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand> */}
                                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                                 <Navbar.Collapse id="responsive-navbar-nav">
                                     <Nav className="mr-auto">
-                                        <Nav.Link href="#features">Features</Nav.Link>
-                                        <Nav.Link href="#pricing">Pricing</Nav.Link>
-                                        <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-                                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                                            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                                            <NavDropdown.Divider />
-                                            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                                        </NavDropdown>
-                                    </Nav>
-                                    <Nav>
-                                        <Nav.Link href="#deets">More deets</Nav.Link>
-                                        <Nav.Link eventKey={2} href="#memes">
-                                            Dank memes
-      </Nav.Link>
+                                        <Nav.Link href="#home" className="navbar-brand hl">Home</Nav.Link>
+                                        <Nav.Link href="#about" className="navbar-brand al">About</Nav.Link>
+                                        <Nav.Link href="#projects" className="navbar-brand pl">Work</Nav.Link>
+                                        <Nav.Link href="#contact" className="navbar-brand cl">Contact</Nav.Link>
+
+                                       
                                     </Nav>
                                 </Navbar.Collapse>
                             </Navbar>
