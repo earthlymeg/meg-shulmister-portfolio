34eacc87c06169b75d02c50c038489c55a05c9d2
Meg Shulmister <megshulmister@Megs-MacBook-Pro.local>
2021-03-12 17:49:09 -0700

    Adding react spinner

--- client/App.js
+++ client/App.js
@@ -6,13 +6,22 @@ import Contact from './components/Contact'
 import Projects from './components/Projects'
 import flower from './images/flower.png';
 import paw from './images/paw.png'
+import ClipLoader from "react-spinners/ClipLoader";
+
 
 const App = () => {
 
     const [currentY, setcurrentY] = useState(0);
     const [headerView, setheaderView] = useState(true);
+    const [isLoading, setIsLoading] = useState(true);
 
-    console.log(paw)
+    useEffect(() => {
+        setTimeout(() => {
+            setIsLoading(false), []
+        }, 1000)
+    }
+    );
+
 
     useEffect(() => {
         window.onscroll = () => {
@@ -21,7 +30,6 @@ const App = () => {
 
         }
         if (currentY >= 50) {
-            console.log('currentY > 54')
             setheaderView(false);
         } else {
             setheaderView(true);
@@ -29,69 +37,81 @@ const App = () => {
     }, [currentY]);
 
 
+    if (!isLoading) {
     return (
+       
+
         <div className="App" >
             {/* <Container fluid> */}
-                <section id="home">
-                    <Row className="justify-content-center">
-                        <Col>
-                            <Navbar collapseOnSelect expand="lg" bg="none" variant="dark">
-                                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                                <Navbar.Collapse id="responsive-navbar-nav">
-                                    <Nav className="mr-auto">
-                                        <Nav.Link href="#home" className="navbar-brand hl">Home</Nav.Link>
-                                        <Nav.Link href="#about" className="navbar-brand al">About</Nav.Link>
-                                        <Nav.Link href="#projects" className="navbar-brand pl">Work</Nav.Link>
-                                        <Nav.Link href="#contact" className="navbar-brand cl">Contact</Nav.Link>
+         
+            <section id="home">
+                <Row className="justify-content-center">
+                    <Col>
+                        <Navbar collapseOnSelect expand="lg" bg="none" variant="dark">
+                            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                            <Navbar.Collapse id="responsive-navbar-nav">
+                                <Nav className="mr-auto">
+                                    <Nav.Link href="#home" className="navbar-brand hl">Home</Nav.Link>
+                                    <Nav.Link href="#about" className="navbar-brand al">About</Nav.Link>
+                                    <Nav.Link href="#projects" className="navbar-brand pl">Work</Nav.Link>
+                                    <Nav.Link href="#contact" className="navbar-brand cl">Contact</Nav.Link>
 
 
-                                    </Nav>
-                                </Navbar.Collapse>
-                            </Navbar>
-                            <div className="d-lg-none sm-screen">
-                                <span className="welcome">Welcome! I am</span>
+                                </Nav>
+                            </Navbar.Collapse>
+                        </Navbar>
+                        <div className="d-lg-none sm-screen">
+                            <span className="welcome">Welcome! I am</span>
 
-                                <br></br> Meg Shulmister <br></br><br></br>
-                                <span className="e">I build web applications.</span>
-                                {/* <span class="fa-stack fa-3x">
-                                <i className="fa fa-heart"></i>
-                                    <span class="fa fa-stack-1x">
-                                        <span className="text-heart">
-                                         
-    </span>
-                                    </span>
-                                </span> */}
-                            </div>
+                            <br></br> Meg Shulmister <br></br><br></br>
+                            <span className="e">I build web applications.</span>
+                            {/* <span class="fa-stack fa-3x">
+            <i className="fa fa-heart"></i>
+            <span class="fa fa-stack-1x">
+            <span className="text-heart">
+            
+            </span>
+            </span>
+        </span> */}
+                        </div>
 
-                        </Col>
-                        {headerView ? <nav class="navbar fixed-bottom header justify-content-end">
-                            <span className="header">Meg <br></br>Shulmister</span>
-                        </nav> : null}
-                        {/* <Col className="flower"> <img src={flower}/>
+                    </Col>
+                    {headerView ? <nav class="navbar fixed-bottom header justify-content-end">
+                        <span className="header">Meg <br></br>Shulmister</span>
+                    </nav> : null}
+                    {/* <Col className="flower"> <img src={flower}/>
+    </Col> */}
+                    {/* <Col className="col2">
+    <div align="right" className="paw">
+    <img src={paw} className="paw"></img>
+    </div>
 </Col> */}
-                        {/* <Col className="col2">
-                            <div align="right" className="paw">
-                                <img src={paw} className="paw"></img>
-                            </div>
-                        </Col> */}
-                    </Row>
-                </section>
+                </Row>
+            </section>
             {/* </Container> */}
             {/* <Container fluid> */}
 
-                <section id="about" >
-                    <About />
-                </section>
-                <section id="projects">
-                    <Projects />
-                </section>
-                <section id="contact">
-                    <Contact />
-                </section>
+            <section id="about" >
+                <About />
+            </section>
+            <section id="projects">
+                <Projects />
+            </section>
+            <section id="contact">
+                <Contact />
+            </section>
 
             {/* </Container> */}
         </div>
+    
+
+)};
+
+if (isLoading) {
+    return (
+        <ClipLoader loading={isLoading} />
     );
+}
 };
 
 export default App;
